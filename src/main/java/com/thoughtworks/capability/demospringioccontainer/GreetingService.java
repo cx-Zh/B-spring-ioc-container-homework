87f71518ae5e1ce406182cec96b151708a76084a
cx-Zh <491907188@qq.com>
2020-06-17 20:52:30 +0800

    p2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 
+    public GreetingService() {
+        System.out.println("++++++++ GreetingService is instantiating...");
+    }
+
     String sayHi() {
         return "hello world";
     }
